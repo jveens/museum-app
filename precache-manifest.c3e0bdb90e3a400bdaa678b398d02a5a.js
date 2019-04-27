0261580cb89003425f559185c53f3ea2ab2250e2
Jenny Veens <jennyveens@gmail.com>
2019-04-27 14:39:26 -0400

    Updates

---
+++ precache-manifest.c3e0bdb90e3a400bdaa678b398d02a5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba72a28bdebd64407509a3a5e64dbce",
+    "url": "/museum-app/index.html"
+  },
+  {
+    "revision": "f3f45be1a5cd5f787ff1",
+    "url": "/museum-app/static/css/main.fbe839e4.chunk.css"
+  },
+  {
+    "revision": "496d96f4d48f7360a369",
+    "url": "/museum-app/static/js/2.50f5a3e5.chunk.js"
+  },
+  {
+    "revision": "f3f45be1a5cd5f787ff1",
+    "url": "/museum-app/static/js/main.7bf6041a.chunk.js"
+  },
+  {
+    "revision": "c2f95ec4949fdd8260ca",
+    "url": "/museum-app/static/js/runtime~main.4a9d3690.js"
+  }
+]);
